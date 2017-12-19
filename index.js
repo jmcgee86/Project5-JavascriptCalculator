8fb1b26dfbe2ca7db757f957c05469507240194f
Jim McGee <jmcgee86@gmail.com>
2017-12-19 00:28:21 +0000

    clicked ints displaying

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /* global $ */
 var classname = document.getElementsByClassName("calcNum");
-
+var clicked = [];
+var display = "";
 var myFunction = function() {
     var val = this.value;
+    clicked.push(val);
+    display = clicked.join("");
+    document.getElementById("disp").innerHTML = display;
     console.log(val);
     };
-//    alert(value);
-//};
 
 for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', myFunction);
