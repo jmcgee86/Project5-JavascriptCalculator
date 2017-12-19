9b404cbafb18ab7064cca17911d04ee5dbabe2bf
Jim McGee <jmcgee86@gmail.com>
2017-12-19 23:05:46 +0000

    clear buttons working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* global $ */
 var classname = document.getElementsByClassName("calcNum");
 var clicked = [];
-var display = "";
+var display = 0;
+var currentTotal = 0;
 var myFunction = function() {
     var val = this.value;
     clicked.push(val);
-    display = clicked.join("");
+    display = parseInt(clicked.join(""));
     document.getElementById("disp").innerHTML = display;
     console.log(val);
     };
@@ -14,3 +15,48 @@ for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', myFunction);
 }
 
+function add(num1, num2){
+    console.log("clicked add")
+    clicked = [];
+    num1=display;
+    display = "";
+    //currentTotal+=display;
+    document.getElementById("disp").innerHTML=display;
+    //display=currentTotal;
+};
+
+function subtract(num1, num2){
+    
+};
+
+function multiply(num1, num2){
+    
+};
+
+function divide(num1, num2){
+    
+};
+
+function clearDisplay(){
+    console.log('clear display clicked');
+    clicked = [];
+    display = 0;
+    document.getElementById("disp").innerHTML = display;
+    
+};
+
+function clearAll(){
+    clicked = [];
+    display = 0;
+    currentTotal = 0;
+    document.getElementById("disp").innerHTML = display;
+    console.log('clear all clicked');
+};
+
+function equals(){
+    console.log('clicked equals')
+    display=currentTotal;
+    document.getElementById("disp").innerHTML=display;
+    clicked = [];
+    
+};
