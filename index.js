782a0e2fdf9d7f168071cdc2f91bfe7dc75725a5
Jim McGee <jmcgee86@gmail.com>
2017-12-20 01:32:12 +0000

    string of addition works, other operators do not

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var currentTotal = 0;
 var myFunction = function() {
     var val = this.value;
     clicked.push(val);
-    display = parseInt(clicked.join(""));
+    display = parseFloat(clicked.join(""));
     document.getElementById("disp").innerHTML = display;
     console.log(val);
     };
@@ -16,25 +16,35 @@ for (var i = 0; i < classname.length; i++) {
 }
 
 function add(num1, num2){
-    console.log("clicked add")
+    console.log("clicked add");
     clicked = [];
-    num1=display;
-    display = "";
-    //currentTotal+=display;
+    currentTotal += display;
+    display = currentTotal;
     document.getElementById("disp").innerHTML=display;
-    //display=currentTotal;
 };
 
 function subtract(num1, num2){
-    
+    console.log("clicked subtract");
+    clicked = [];
+    currentTotal -= display;
+    display = currentTotal;
+    document.getElementById("disp").innerHTML=display;
 };
 
 function multiply(num1, num2){
-    
+    console.log("clicked multiply");
+    clicked = [];
+    currentTotal *= display;
+    display = currentTotal;
+    document.getElementById("disp").innerHTML=display;
 };
 
 function divide(num1, num2){
-    
+    console.log("clicked divide");
+    clicked = [];
+    currentTotal /= display;
+    display = currentTotal;
+    document.getElementById("disp").innerHTML=display;
 };
 
 function clearDisplay(){
