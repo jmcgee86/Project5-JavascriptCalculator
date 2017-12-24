3374e4590406c395716f994ffa2b3b49b42c729b
Jim McGee <jmcgee86@gmail.com>
2017-12-24 02:25:10 +0000

    minimum requirements met

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
 var numClass = document.getElementsByClassName("calcNum");
 var opClass = document.getElementsByClassName("operators");
 var display = document.getElementById("disp");
-var equation = [];
+var allInputs = [];
 var numInput = 0;
 var runningTotal = 0;
-
-var numberClick = function () {
-    var numVal = this.value
-    if (numInput == 0){
-        numInput = numVal;
-        } else{
-    numInput += numVal;
-        }
-    display.innerHTML = numInput
-    console.log(equation)
-    };
-var operatorClick = function(){
-    console.log(equation);
-    equation.push(numInput);
-    numInput = 0;
-    var opVal = this.value;
-    equation.push(opVal);
+var numberClick = function() {
+	var numVal = this.value;
+	if (numInput == 0) {
+		numInput = numVal;
+	} else {
+		numInput += numVal;
+	}
+	display.innerHTML = numInput;
+};
+var operatorClick = function() {
+	var opVal = this.value;
+	equals();
+	allInputs.push(opVal);
 };
 
-function equals(){
-    equation.push(numInput);
-    console.log(equation);
-    for (var i = 0; i<equation.length; i++){
-        if (equation[i] == "add") {
-				runningTotal += parseFloat(equation[i + 1], 10);
-				i++;
-				console.log("after addition, result is " + runningTotal);
-			} else if (equation[i] == "subtract") {
-				runningTotal -= parseFloat(equation[i + 1],10);
-				i ++;
-				console.log("after subtraction, result is " + runningTotal);
-			} else if (equation[i] == "multiply") {
-				runningTotal *= parseFloat(equation[i + 1],10);
-				i ++;
-				console.log("after multiplication, result is " + runningTotal);
-			} else if (equation[i] == "divide") {
-				runningTotal /= parseFloat(equation[i + 1], 10);
-				i ++;
-				console.log("after division, result is " + runningTotal);
-			} else{  
-				runningTotal += parseFloat(equation[i], 10);
-			}
+function equals() {
+	allInputs.push(numInput);
+	for (var i = 0; i < allInputs.length; i++) {
+		if (allInputs[i] == "add") {
+			runningTotal += parseFloat(allInputs[i + 1], 10);
+			i++;
+		} else if (allInputs[i] == "subtract") {
+			runningTotal -= parseFloat(allInputs[i + 1], 10);
+			i++;
+		} else if (allInputs[i] == "multiply") {
+			runningTotal *= parseFloat(allInputs[i + 1], 10);
+			i++;
+		} else if (allInputs[i] == "divide") {
+			runningTotal /= parseFloat(allInputs[i + 1], 10);
+			i++;
+		} else {
+			runningTotal += parseFloat(allInputs[i], 10);
 		}
-		equation = [];
-		numInput = 0;
-		display.innerHTML = runningTotal;
-    }
-
+	}
+	allInputs = [];
+	numInput = 0;
+	display.innerHTML = runningTotal;
+}
 //adds numberClick function to number buttons of calculator
 for (var i = 0; i < numClass.length; i++) {
-    numClass[i].addEventListener('click', numberClick);
+	numClass[i].addEventListener('click', numberClick);
 }
 //adds operatorClick function to operator buttons of calculator
-for (var i=0; i<opClass.length; i++){
-    opClass[i].addEventListener('click', operatorClick);
+for (var i = 0; i < opClass.length; i++) {
+	opClass[i].addEventListener('click', operatorClick);
 }
-
-document.getElementById("eq").addEventListener('click', equals());
+document.getElementById("ce").addEventListener('click', function() {
+	allInputs = [];
+	numInput = 0;
+	runningTotal = 0;
+	display.innerHTML = 0;
+});
+document.getElementById("eq").addEventListener('click', equals);
+document.getElementById("clear").addEventListener('click', function() {
+	numInput = 0;
+	display.innerHTML = runningTotal;
+});
