b24aa20dd7fa0c6a58f4444186523a035e332186
Jim McGee <jmcgee86@gmail.com>
2017-12-24 00:14:18 +0000

    basic functions operators all  working

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
-/* global $ */
-var classname = document.getElementsByClassName("calcNum");
-var clicked = [];
-var display = 0;
-var currentTotal = 0;
-var myFunction = function() {
-    var val = this.value;
-    clicked.push(val);
-    display = parseFloat(clicked.join(""));
-    document.getElementById("disp").innerHTML = display;
-    console.log(val);
+var numClass = document.getElementsByClassName("calcNum");
+var opClass = document.getElementsByClassName("operators");
+var display = document.getElementById("disp");
+var equation = [];
+var numInput = 0;
+var runningTotal = 0;
+
+var numberClick = function () {
+    var numVal = this.value
+    if (numInput == 0){
+        numInput = numVal;
+        } else{
+    numInput += numVal;
+        }
+    display.innerHTML = numInput
+    console.log(equation)
     };
-
-for (var i = 0; i < classname.length; i++) {
-    classname[i].addEventListener('click', myFunction);
-}
-
-function add(num1, num2){
-    console.log("clicked add");
-    clicked = [];
-    currentTotal += display;
-    display = currentTotal;
-    document.getElementById("disp").innerHTML=display;
-};
-
-function subtract(num1, num2){
-    console.log("clicked subtract");
-    clicked = [];
-    currentTotal -= display;
-    display = currentTotal;
-    document.getElementById("disp").innerHTML=display;
-};
-
-function multiply(num1, num2){
-    console.log("clicked multiply");
-    clicked = [];
-    currentTotal *= display;
-    display = currentTotal;
-    document.getElementById("disp").innerHTML=display;
-};
-
-function divide(num1, num2){
-    console.log("clicked divide");
-    clicked = [];
-    currentTotal /= display;
-    display = currentTotal;
-    document.getElementById("disp").innerHTML=display;
-};
-
-function clearDisplay(){
-    console.log('clear display clicked');
-    clicked = [];
-    display = 0;
-    document.getElementById("disp").innerHTML = display;
-    
-};
-
-function clearAll(){
-    clicked = [];
-    display = 0;
-    currentTotal = 0;
-    document.getElementById("disp").innerHTML = display;
-    console.log('clear all clicked');
+var operatorClick = function(){
+    console.log(equation);
+    equation.push(numInput);
+    numInput = 0;
+    var opVal = this.value;
+    equation.push(opVal);
 };
 
 function equals(){
-    console.log('clicked equals')
-    display=currentTotal;
-    document.getElementById("disp").innerHTML=display;
-    clicked = [];
-    
-};
+    equation.push(numInput);
+    console.log(equation);
+    for (var i = 0; i<equation.length; i++){
+        if (equation[i] == "add") {
+				runningTotal += parseFloat(equation[i + 1], 10);
+				i++;
+				console.log("after addition, result is " + runningTotal);
+			} else if (equation[i] == "subtract") {
+				runningTotal -= parseFloat(equation[i + 1],10);
+				i ++;
+				console.log("after subtraction, result is " + runningTotal);
+			} else if (equation[i] == "multiply") {
+				runningTotal *= parseFloat(equation[i + 1],10);
+				i ++;
+				console.log("after multiplication, result is " + runningTotal);
+			} else if (equation[i] == "divide") {
+				runningTotal /= parseFloat(equation[i + 1], 10);
+				i ++;
+				console.log("after division, result is " + runningTotal);
+			} else{  
+				runningTotal += parseFloat(equation[i], 10);
+			}
+		}
+		equation = [];
+		numInput = 0;
+		display.innerHTML = runningTotal;
+    }
+
+//adds numberClick function to number buttons of calculator
+for (var i = 0; i < numClass.length; i++) {
+    numClass[i].addEventListener('click', numberClick);
+}
+//adds operatorClick function to operator buttons of calculator
+for (var i=0; i<opClass.length; i++){
+    opClass[i].addEventListener('click', operatorClick);
+}
+
+document.getElementById("eq").addEventListener('click', equals());
